68a0cdc230b0eb7921552cfd4aca1a5ac0d29df0
Jhrohman <josephhrohman@gmail.com>
2019-06-13 11:30:39 -0700

    updating code. cleaning time

--- src/components/layout/profile/profile.js
+++ src/components/layout/profile/profile.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import ProfileBlock from '../../replicated/profileBlock/profileBlock';
 import ToolBelt from '../../replicated/toolBelt/toolBelt';
 import axios from 'axios';
 import './profile.css';
 
-//{ match, deletePost, currentUser }
-
 const Profile = ({ currentUser }) => {
   const [ error, setError ] = useState(null),
-        // [ user, setUser ] = useState({}),
         [ userInfo, setUserInfo ] = useState({}),
         [ toolBelt, setToolBelt ] = useState([]),
         [ userProjects, setUserProjects ] = useState([]),
@@ -36,34 +32,26 @@ const Profile = ({ currentUser }) => {
   const getProfile = async () => {
         try {
           const response = await axios.get(`http://localhost:4000/api/v1/users/${localStorage.currentUser}`);
-          console.table(response.data.user);
           setUserInfo(response.data.user);
-          console.log(currentUser);
         } catch(err) {
-          console.log(err);
           setError(err.response.data.error);
+          console.log(error ? error : null);
         };
   }
   
   const getToolBelt = async () => {
         const response = await axios.get(`http://localhost:4000/api/v1/tools`);
-        console.table(response.data);
         setToolBelt(response.data);
-        console.log({tools});
   }
 
   const getProjects = async () => {
         const response = await axios.get(`http://localhost:4000/api/v1/projects`);
-        console.table(response.data);
         setUserProjects(response.data);
-        console.table({userProjects});
   }
   
   const getTools = async () => {
         const response = await axios.get(`http://localhost:4000/api/v1/tools`);
-        console.table(response.data);
         setTools(response.data);
-        console.table({tools});
   }
 
   return(
@@ -97,16 +85,3 @@ const Profile = ({ currentUser }) => {
 }
 
 export default Profile;
-
-//.filter((point) => {return point.user_id == userInfo._id})
-
-  // const heroes = [
-  //   {name: “Batman”, franchise: “DC”},
-  //   {name: “Ironman”, franchise: “Marvel”},
-  //   {name: “Thor”, franchise: “Marvel”},
-  //   {name: “Superman”, franchise: “DC”}
-  // ];
-  
-  // var marvelHeroes =  heroes.filter(function(hero) {
-  //   return hero.franchise == “Marvel”;
-  // });
